cbc865651670a6b303c55e96e04eb2b0fcbf37c8
Junjie14321 <143826592+Junjie14321@users.noreply.github.com>
2025-08-06 11:16:52 +0000

    Added go up button

--- static/index.js
+++ static/index.js
@@ -2,3 +2,18 @@ AOS.init({
     duration: 1000, // animation duration in ms
     once: false,     // only animate once when scrolling down
 });
+
+// Show the button after scrolling down 100px
+window.onscroll = function () {
+const btn = document.getElementById("backToTopBtn");
+if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    btn.style.display = "block";
+} else {
+    btn.style.display = "none";
+}
+};
+
+function scrollToTop() {
+window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
